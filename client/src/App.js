aa73c11033968eef9feff77c7a0120b36107829c
Marcin Skowronek <marcinfoto35@op.pl>
2022-01-07 09:22:29 +0100

    Working select element - one problem

--- client/src/App.js
+++ client/src/App.js
@@ -17,35 +17,40 @@ class App extends React.Component {
     };
   }
 
-  render() {
-    const handleChange = (e) => {
-      this.setState({ selectedValue: e.target.value });
-    };
-    const handleChangeName = (e, id) => {
-      // let index = this.state.args.find(item => item.id === id);
-      // console.log(index);
-      // console.log(e.target.value);
-      // console.log(id);
+  handleChange = (e) => {
+    this.setState({ selectedValue: e.target.value });
+  };
 
-      const newObj = this.state.args.map(obj => {
-        if ([id].includes(obj.id)) {
-          return {...obj, arg: e.target.value}
-        }
-        return obj
-      })
-      console.log(newObj);
-      // this.setState({args: [newObj]})
-      // let check = this.state.args.splice(id,1,e.target.value);
-      // console.log(check);
-      // this.setState({args: [check, ...this.state.args]
-      // })
-      this.setState({ args: newObj });
-    };
-    const handleAddVar = (elem) => {
-      this.setState({
-        args: [...this.state.args, elem],
-      })
-    }
+  handleChangeName = (e, id) => {
+
+    const newArray = this.state.args.map(array => {
+      if ([id].includes(array.id)) {
+        return { ...array, arg: e.target.value }
+      }
+      return array
+    })
+    console.log(newArray);
+    this.setState({ args: newArray });
+  };
+
+  handleChangeLog = (e, id) => {
+
+    const newArray = this.state.args.map(array => {
+      if ([id].includes(array.id)) {
+        return { ...array, logVar: e.target.value }
+      }
+      return array
+    })
+    console.log(newArray);
+    this.setState({ args: newArray });
+  };
+
+  handleAddVar = (elem) => {
+    this.setState({
+      args: [...this.state.args, elem],
+    })
+  }
+  render() {
     console.log(this.state);
     return (
       <div className="app">
@@ -54,10 +59,13 @@ class App extends React.Component {
             <div key={item.id}>
               <input
                 autoComplete="off"
-                onChange={(e) => handleChangeName(e, item.id)}
+                onChange={(e) => this.handleChangeName(e, item.id)}
                 value={this.state.args[index].arg}
               />
-              <select onChange={(e) => handleChange(e)}>
+              <select onChange={(e) => {
+                // this.handleChange(e);
+                this.handleChangeLog(e, item.id);
+              }}>
                 {this.props.options.map((arrayItem, index) => (
                   <option key={index} value={arrayItem}>
                     {arrayItem}
@@ -70,7 +78,7 @@ class App extends React.Component {
         <button onClick={e => {
           e.preventDefault();
           let id = randomID(4);
-          handleAddVar({ id: id, arg: this.state.variableName, logVar: this.state.selectedValue });
+          this.handleAddVar({ id: id, arg: this.state.variableName, logVar: this.state.selectedValue });
         }} >+ add arg</button>
         <div className="result">Result</div>
       </div>
